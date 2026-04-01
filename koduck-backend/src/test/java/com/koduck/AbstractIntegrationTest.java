b9845e3ff2fa87110d9543046043607770615058
hailingu <linhaigu@gmail.com>
2026-04-01 15:08:14 +0800

    test(integration): align datasource driver with testcontainers postgres

--- koduck-backend/src/test/java/com/koduck/AbstractIntegrationTest.java
+++ koduck-backend/src/test/java/com/koduck/AbstractIntegrationTest.java
@@ -32,6 +32,7 @@ public abstract class AbstractIntegrationTest {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
         registry.add("spring.flyway.enabled", () -> "true");
     }
 }
