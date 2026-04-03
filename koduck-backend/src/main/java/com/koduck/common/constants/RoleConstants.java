39694579f137d040fb1bfe991ddf8fbb467b836d
hailingu <linhaigu@gmail.com>
2026-04-03 16:20:03 +0800

    style: add @author tag and field Javadocs in RoleConstants

--- koduck-backend/src/main/java/com/koduck/common/constants/RoleConstants.java
+++ koduck-backend/src/main/java/com/koduck/common/constants/RoleConstants.java
@@ -2,10 +2,19 @@ package com.koduck.common.constants;
 
 /**
  * Shared role-related constants.
+ *
+ * @author Koduck Team
  */
 public final class RoleConstants {
 
+    /**
+     * Default user role name.
+     */
     public static final String DEFAULT_USER_ROLE_NAME = "USER";
+
+    /**
+     * Default user role description.
+     */
     public static final String DEFAULT_USER_ROLE_DESCRIPTION = "Default role for regular users";
 
     private RoleConstants() {
